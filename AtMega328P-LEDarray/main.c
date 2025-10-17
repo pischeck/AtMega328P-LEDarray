07c7ea11cdcdc58d43fd3ce0db3a26b224847dd5
piszczek.jakub03@gmail.com <piszczek.jakub03@gmail.com>
2025-10-17 12:30:52 +0200

    Initial commit - ATmega328P project created in Microchip Studio

---
+++ AtMega328P-LEDarray/main.c
@@ -0,0 +1,18 @@
+/*
+ * AtMega328P-LEDarray.c
+ *
+ * Created: 17.10.2025 12:26:39
+ * Author : piszc
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
